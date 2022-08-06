4cca55b10876370a70ed0af449935163e3aeaeee
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-06 12:05:03 +0300

    create functions to set and get state from localStorage

---
+++ src/store/loaclStorage.ts
@@ -0,0 +1,23 @@
+import {ReduxStateType} from './store';
+
+export const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('state')
+        if (serializedState === null) {
+            return undefined
+        }
+        return JSON.parse(serializedState)
+    } catch (err) {
+        return undefined
+    }
+}
+
+export const saveState = (state: ReduxStateType) => {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    } catch (err) {
+        console.log(err)
+        //Or ignore write errors.
+    }
+}
