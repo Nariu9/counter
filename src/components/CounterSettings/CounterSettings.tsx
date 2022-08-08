a4069cd573cfca8b192cea5a332ff9764fb5e6dc
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-08 21:03:59 +0300

    refactor CounterSettings component to avoid setting wrong values to store

--- src/components/CounterSettings/CounterSettings.tsx
+++ src/components/CounterSettings/CounterSettings.tsx
@@ -1,7 +1,7 @@
-import classes from "../../App.module.css";
-import {NumberInput} from "../NumberInput/NumberInput";
-import {Button} from "../Button/Button";
-import React from "react";
+import classes from '../../App.module.css';
+import {NumberInput} from '../NumberInput/NumberInput';
+import {Button} from '../Button/Button';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {ReduxStateType} from '../../state/store';
 import {
@@ -14,32 +14,36 @@ import {
 } from '../../state/counter-reducer';
 
 export const CounterSettings = () => {
+
     const counterState = useSelector<ReduxStateType, CounterStateType>(state => state.counter)
     const {start, max, error, editMode} = counterState
     const dispatch = useDispatch()
+    const [_start, _setStart] = useState<number>(start)
+    const [_max, _setMax] = useState<number>(max)
 
     const maxValueHandler = (value: number) => {
+        _setMax(value)
         !editMode && dispatch(setEditModeAC(true))
-        dispatch(setMaxAC(value))
-        if (value < 0 || value <= start || start < 0) {
-            dispatch(setErrorAC(true))
-            return
+        if (value > 0 && value > _start && _start >= 0) {
+            dispatch(setMaxAC(value))
+            error && dispatch(setErrorAC(false))
+        } else {
+            !error && dispatch(setErrorAC(true))
         }
-        error && dispatch(setErrorAC(false))
     }
     const startValueHandler = (value: number) => {
+        _setStart(value)
         !editMode && dispatch(setEditModeAC(true))
-        dispatch(setStartAC(value))
-        if (value < 0 || value >= max) {
-            dispatch(setErrorAC(true))
-            return
+        if (value >= 0 && value < _max) {
+            dispatch(setStartAC(value))
+            error && dispatch(setErrorAC(false))
+        } else {
+            !error && dispatch(setErrorAC(true))
         }
-        error && dispatch(setErrorAC(false))
     }
     const setValuesHandler = () => {
         dispatch(resetToStartAC())
         dispatch(setEditModeAC(false))
-
     }
 
 
@@ -48,15 +52,15 @@ export const CounterSettings = () => {
             <div className={`${classes.content} ${classes.values}`}>
                 <div className={classes.value}>
                     <span>max value:</span>
-                    <NumberInput value={max}
+                    <NumberInput value={_max}
                                  callback={maxValueHandler}
-                                 className={max < 0 || max <= start ? classes.inputError : ''}/>
+                                 className={_max < 0 || _max <= _start ? classes.inputError : ''}/>
                 </div>
                 <div className={classes.value}>
                     <span>start value:</span>
-                    <NumberInput value={start}
+                    <NumberInput value={_start}
                                  callback={startValueHandler}
-                                 className={start < 0 || start >= max ? classes.inputError : ''}/>
+                                 className={_start < 0 || _start >= _max ? classes.inputError : ''}/>
                 </div>
             </div>
             <div className={classes.buttonContainer}>
