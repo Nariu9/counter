ac8a464a389c3237f3fb504ddea6f3a62c38a1aa
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-08 09:03:49 +0300

    get editMode value from state

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,15 @@ import {CounterSettings} from './components/CounterSettings/CounterSettings';
 import {Counter} from './components/Counter/Counter';
 import {useSelector} from 'react-redux';
 import {ReduxStateType} from './store/store';
-import {
-    CounterStateType,
-} from './store/counter-reducer';
 
 
 function App() {
 
-    const state = useSelector<ReduxStateType, CounterStateType>(state => state.counter)
+    const editMode = useSelector<ReduxStateType, boolean>(state => state.counter.editMode)
 
     return (
         <div className={classes.App}>
-            {state.editMode ? <CounterSettings/> : <Counter/>}
+            {editMode ? <CounterSettings/> : <Counter/>}
         </div>
     );
 }
