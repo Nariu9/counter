ef4f531dc92974786fff2e457737e68282ca4669
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-08 13:37:03 +0300

    wrap App in Provider and subscribe Local Storage state to the store changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {store} from './state/store';
+import {saveState} from './state/localStorage';
+import throttle from 'lodash.throttle';
+
+
+store.subscribe(throttle(()=>{
+    saveState(store.getState())
+}, 1000))
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
