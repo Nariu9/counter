6d183d0d85486ab17b7a08100cd1a8c38c6dad32
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-06 09:14:07 +0300

    refactor counter reducer

--- src/state/counter-reducer.ts
+++ src/state/counter-reducer.ts
@@ -23,26 +23,23 @@ const initialState: CounterStateType = {
 
 export const counterReducer = (state: CounterStateType = initialState, action: ActionType): CounterStateType => {
     switch (action.type) {
-        case 'SET-START':
-            return {...state, start: action.start}
-        case 'SET-MAX':
-            return {...state, max: action.max}
         case 'INCREMENT':
             return {...state, count: state.count + 1}
         case 'RESET-TO-START':
             return {...state, count: state.start}
+        case 'SET-START':
+        case 'SET-MAX':
         case 'SET-ERROR':
-            return {...state, error: action.error}
         case 'SET-EDIT-MODE':
-            return {...state, editMode: action.editMode}
+            return {...state, ...action.payload}
         default:
             return state
     }
 }
 
-export const setStartAC = (start: number) => ({type: 'SET-START', start}) as const
-export const setMaxAC = (max: number) => ({type: 'SET-MAX', max}) as const
 export const incrementAC = () => ({type: 'INCREMENT'}) as const
 export const resetToStartAC = () => ({type: 'RESET-TO-START'}) as const
-export const setErrorAC = (error: boolean) => ({type: 'SET-ERROR', error}) as const
-export const setEditModeAC = (editMode: boolean) => ({type: 'SET-EDIT-MODE', editMode}) as const
+export const setStartAC = (start: number) => ({type: 'SET-START', payload: {start}}) as const
+export const setMaxAC = (max: number) => ({type: 'SET-MAX', payload: {max}}) as const
+export const setErrorAC = (error: boolean) => ({type: 'SET-ERROR', payload: {error}}) as const
+export const setEditModeAC = (editMode: boolean) => ({type: 'SET-EDIT-MODE', payload: {editMode}}) as const
