3752147f5a913c8c257fe9d6277a38494f8268d4
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-06 11:59:41 +0300

    refactor counter reducer, add thunk

--- src/store/counter-reducer.ts
+++ src/store/counter-reducer.ts
@@ -1,18 +1,6 @@
-export type CounterStateType = {
-    start: number
-    max: number
-    count: number
-    error: boolean
-    editMode: boolean
-}
-type ActionType = ReturnType<typeof setStartAC>
-    | ReturnType<typeof setMaxAC>
-    | ReturnType<typeof incrementAC>
-    | ReturnType<typeof resetAC>
-    | ReturnType<typeof setErrorAC>
-    | ReturnType<typeof toggleSettingsAC>
+import {AppThunk} from './store';
 
-const initialState: CounterStateType = {
+const initialState = {
     start: 0,
     max: 0,
     count: 0,
@@ -20,14 +8,14 @@ const initialState: CounterStateType = {
     editMode: false
 }
 
-export const counterReducer = (state: CounterStateType = initialState, action: ActionType): CounterStateType => {
+export const counterReducer = (state: CounterStateType = initialState, action: ActionTypes): CounterStateType => {
     switch (action.type) {
         case 'SET-START':
             return {...state, start: action.value}
         case 'SET-MAX':
             return {...state, max: action.value}
         case 'INCREMENT':
-            return {...state, count: action.value + 1}
+            return {...state, count: state.count + 1}
         case 'RESET' :
             return {...state, count: state.start}
         case 'SET-ERROR':
@@ -39,9 +27,39 @@ export const counterReducer = (state: CounterStateType = initialState, action: A
     }
 }
 
+// actions
 export const setStartAC = (value: number) => ({type: 'SET-START', value}) as const
 export const setMaxAC = (value: number) => ({type: 'SET-MAX', value}) as const
-export const incrementAC = (value: number) => ({type: 'INCREMENT', value}) as const
+export const incrementAC = () => ({type: 'INCREMENT'}) as const
 export const resetAC = () => ({type: 'RESET'}) as const
 export const setErrorAC = (isError: boolean) => ({type: 'SET-ERROR', isError}) as const
 export const toggleSettingsAC = (toggleSettings: boolean) => ({type: 'TOGGLE-SETTINGS', toggleSettings}) as const
+
+// thunk
+export const setStartToLSTC = (value: number): AppThunk => (dispatch) => {
+    localStorage.setItem('startValue', JSON.stringify(value))
+    dispatch(setStartAC(value))
+}
+export const setMaxToLSTC = (value: number): AppThunk => (dispatch) => {
+    localStorage.setItem('maxValue', JSON.stringify(value))
+    dispatch(setMaxAC(value))
+}
+export const getStartFromLSTC = ():AppThunk => (dispatch)=> {
+    const startString = localStorage.getItem('startValue')
+    startString && dispatch(setStartAC(JSON.parse(startString)))
+    dispatch(resetAC())
+}
+export const getMaxFromLSTC = ():AppThunk => (dispatch)=> {
+    const maxString = localStorage.getItem('maxValue')
+    maxString && dispatch(setMaxAC(JSON.parse(maxString)))
+}
+
+// types
+export type ActionTypes = ReturnType<typeof setStartAC>
+    | ReturnType<typeof setMaxAC>
+    | ReturnType<typeof incrementAC>
+    | ReturnType<typeof resetAC>
+    | ReturnType<typeof setErrorAC>
+    | ReturnType<typeof toggleSettingsAC>
+
+export type CounterStateType = typeof initialState
